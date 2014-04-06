d91060f5188c8801c99227bf769357a6d49f6b3a
himanshu5692 <himanshu5692@gmail.com>
2014-04-06 12:24:09 +0530

    Test
    ReTest

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?php
+<?php 
+$DB = new PDO("mysql:dbname='test';host='127.5.166.130:3306'", 'admin6XgqKBd', '9sCpyk_VwKEh'); 
 
-echo "Hello";
+$order = "select * from friendList where FriendId='1' and IdGivenBy='Mobile'";
+$result = $DB->prepare($sql);
+
+$row1 = mysqli_fetch_array($result);
+$getName = $row1["FriendName"];
+
+echo $getName;
 
 ?>
