0940f56b8f8565cc8eb363fa2d04d2a6d1f9e592
himanshu5692 <himanshu5692@gmail.com>
2014-04-06 12:42:43 +0530

    ReTest
    Changed DB Connecttion

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php 
-$DB = new PDO("mysql:dbname='test';host='127.5.166.130:3306'", 'admin6XgqKBd', '9sCpyk_VwKEh'); 
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_BASE','test');
+define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+
+$dsn = 'mysql:dbname='.DB_BASE.';host='.DB_HOST.';port='.DB_PORT;
+$dbh = new PDO($dsn, DB_USER, DB_PASS);
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $order = "select * from friendList where FriendId='1' and IdGivenBy='Mobile'";
 $result = $DB->prepare($sql);
